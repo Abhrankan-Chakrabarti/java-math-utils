7835a50da66f2c2273d65f1db073ad29ff82fe48
Abhrankan Chakrabarti <abhrankan@gmail.com>
2024-05-13 16:32:08 +0530

    Update SqrtBenchmark.java

--- SqrtBenchmark.java
+++ SqrtBenchmark.java
@@ -8,16 +8,27 @@ public class SqrtBenchmark {
         BigDecimal number = new BigDecimal("2"); // Number to calculate square root of
 
         // Benchmark BigDecimal in-built sqrt
-        BigDecimal sqrt1 = number.sqrt(new MathContext(digits, RoundingMode.HALF_UP));
+        long startTime = System.currentTimeMillis();
+        BigDecimal sqrt1 = number.sqrt(new MathContext(digits + 1, RoundingMode.HALF_UP))
+                                  .setScale(digits, RoundingMode.HALF_UP);
+        long endTime = System.currentTimeMillis();
+        long timeBigDecimal = endTime - startTime;
 
         // Benchmark pell_sqrt
-        BigDecimal sqrt2 = new pell_sqrt().pell_sqrt(2, digits);
+        startTime = System.currentTimeMillis();
+        BigDecimal sqrt2 = new pell_sqrt().pell_sqrt(2, digits + 1)
+                                          .setScale(digits, RoundingMode.HALF_UP);
+        endTime = System.currentTimeMillis();
+        long timePellSqrt = endTime - startTime;
 
-        // Debugging: Print the values of sqrt1 and sqrt2
-        System.out.println("BigDecimal sqrt: " + sqrt1.toString());
-        System.out.println("Pell sqrt: " + sqrt2.toString());
+        System.out.println("BigDecimal sqrt result: " + sqrt1);
+        System.out.println("Pell sqrt result: " + sqrt2);
 
         // Check if the results are the same
         System.out.println("Results are equal: " + sqrt1.equals(sqrt2));
+
+        // Print benchmark times
+        System.out.println("BigDecimal sqrt time: " + timeBigDecimal + " ms");
+        System.out.println("Pell sqrt time: " + timePellSqrt + " ms");
     }
 }
